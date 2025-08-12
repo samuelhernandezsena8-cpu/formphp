87d3634c7776f25bd5e9ec6f6aae71329d9a2c02
samuelhernandezsena8-cpu <samuelhernandezsena8@gmail.com>
2025-08-12 16:10:07 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,12 @@
+<?php
+$identificacion= $_POST ['identificacion'];
+$NomApellidos=$_POST ['NomApellidos'];
+$Cargo=$_POST['Cargo'];
+$Salario=$_POST ['Salario'];
+echo "La identificacion del empleado es: ".identificacion;
+echo "Nombres y Apellidos del empleado: ".NomApellidos;
+echo "Cargo del empleado: ".Cargo;
+echo "Salario del empleado: ".Salario;
+?>
+
 
